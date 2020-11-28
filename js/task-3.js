a2b5fd5b3001cc80d4cdb89cc7769d34eeb87802
Andrii_Babenko <andrey.brnr@gmail.com>
2020-11-28 10:04:35 +0200

    task-3 DONE

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+const ADMIN_PASSWORD = 'jqueryismyjam';
+let message;
+message = prompt(`Введите пароль администратора`);
+
+if (message === ADMIN_PASSWORD) {
+    alert(`Добро пожаловать!`);
+}
+else if (message === null) {
+    alert(`Отменено пользователем!`);
+}
+else  {
+  alert(`Доступ запрещен, неверный пароль!`);
+}

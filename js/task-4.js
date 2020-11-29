1583b7a9834d54565c5fb2296f5bee241c9c8f8e
Andrii_Babenko <andrey.brnr@gmail.com>
2020-11-29 10:47:23 +0200

    FIX task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,19 +1,25 @@
 const credits = 23580;
 const pricePerDroid = 3000;
+let message;
 
 let totalPrice = prompt(`Введите кол-во дроидов, которые хотите купить. Стоимость 1 дроида ${pricePerDroid} кредитов. Ваш баланс ${credits} кредитов`);
 
 if (totalPrice === null) {
-    alert(`Отменено пользователем!`);
+    message = 'Отменено пользователем!';
 }
 else if (totalPrice * pricePerDroid > credits) {
 
-alert(`Недостаточно средств на счету!`);      
+    message = 'Недостаточно средств на счету!';      
 }  
 else {
     totalPrice = Number(totalPrice);
-    alert(`Вы купили ${totalPrice} дроидов, на счету осталось ${credits - totalPrice * pricePerDroid} кредитов.`);
+    const finalCredits = credits - totalPrice * pricePerDroid
+    message = `Вы купили ${totalPrice} дроидов, на счету осталось ${finalCredits} кредитов.`;
     
 }
+
+console.log(message);
+
+
 
 
